b8cd53948168572673888782ec15bdfe130ad35c
Priyanka <priyankapotlia03@gmail.com>
2025-05-26 16:29:18 +0530

    Updated FeedbackRoutes.js

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const express = require('express');
 const router = express.Router();
 const feedbackController = require('../controllers/feedbackController');
 
+// Create feedback
 router.post('/', feedbackController.createFeedback);
+
+// Get all feedback
 router.get('/', feedbackController.getAllFeedback);
+
+// Get feedback by ID
 router.get('/:id', feedbackController.getFeedbackById);
+
+// Get feedback by event ID
+router.get('/event/:eventId', feedbackController.getFeedbackByEvent);
+
+// Delete feedback
 router.delete('/:id', feedbackController.deleteFeedback);
 
 module.exports = router;
